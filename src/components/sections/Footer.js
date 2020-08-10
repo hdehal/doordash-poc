abbe392f332d58697796db4d3ad7952dee4ca354
hdehal <harry@hdehal.com>
2020-08-10 23:30:45 +0000

    Add footer site map and app store buttons

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -4,14 +4,56 @@ import facebook from '@images/logos/facebook.svg';
 import twitter from '@images/logos/instagram.svg';
 import instagram from '@images/logos/twitter.svg';
 import dasherIcon from '@images/logos/dasher_icon.svg';
+import appleStore from '@images/logos/apple_store.svg';
+import googleStore from '@images/logos/google_store.svg';
 
 export default () => (
 
   <div className="copyrightContainer">
+
+    <ul className="siteMap">
+      <h4>Get to Know Us</h4>
+      <li><a href="https://www.doordash.com/about/">About Us</a></li>
+      <li><a href="https://www.doordash.com/careers/">Careers</a></li>
+      <li><a href="http://blog.doordash.com/">Blog</a></li>
+      <li><a href="https://www.linkedin.com/company/doordash">LinkedIn</a></li>
+      <li><a href="https://www.glassdoor.com/Reviews/DoorDash-Reviews-E813073.htm">Glassdoor</a></li>
+      <li><a href="https://www.doordash.com/accessibility/">Accessibility</a></li>
+    </ul>
+
+    <ul className="siteMap">
+      <h4>Let Us Help You</h4>
+      <li><a href="https://www.doordash.com/consumer/edit_profile/">Account Details</a></li>
+      <li><a href="https://www.doordash.com/orders/">Order History</a></li>
+      <li><a href="https://support.doordash.com/">Support</a></li>
+      <li><a href="https://support.doordash.com/#/general">FAQs</a></li>
+    </ul>
+
+    <ul className="siteMap">
+      <h4>Doing Business</h4>
+      <li><a href="https://www.doordash.com/dasher/signup/">Become a Dasher</a></li>
+      <li><a href="https://www.doordash.com/merchant/apply/">Be a Partner Restaurant</a></li>
+      <li><a href="https://www.doordash.com/drive/discover/">Get Dashers for Deliveries</a></li>
+    </ul>
+
+    <ul className="getApp">
+      <li>
+        <a href="https://itunes.apple.com/us/app/doordash-food-delivery/id719972451?mt=8" target="_blank" rel="noreferrer" class="custom-footer-logo">
+          <img src={appleStore} alt="Available on the App Store" />
+        </a>
+      </li>
+      <li>
+        <a href="https://play.google.com/store/apps/details?id=com.dd.doordash" target="_blank" rel="noreferrer" class="custom-footer-logo">
+          <img src={googleStore} alt="Android App on Google Play" />
+        </a>
+      </li>
+    </ul>
+
+
     <div className="custom-copyright">
       <ul>
         <li>
-          <img src={dasherIcon} /><a href="https://www.doordash.com/" class="custom-footer-logo">
+          <img src={dasherIcon} alt="DoorDash" /><a href="https://www.doordash.com/" class="custom-footer-logo">
           </a>
         </li>
         <li>
@@ -25,28 +67,28 @@ export default () => (
         </li>
         <li>
           &copy; 2019 DoorDash
-                  </li>
+      </li>
       </ul>
     </div>
     <div className="custom-copyright social">
       <ul>
         <li>
-          <a href="http://facebook.com/doordash" target="_blank" class="custom-footer-logo">
-            <img src={facebook} />
+          <a href="http://facebook.com/doordash" target="_blank" rel="noreferrer" class="custom-footer-logo">
+            <img src={facebook} alt="Facebook" />
           </a>
         </li>
         <li>
-          <a href="http://twitter.com/doordash" target="_blank" class="custom-footer-logo">
-            <img src={twitter} />
+          <a href="http://twitter.com/doordash" target="_blank" rel="noreferrer" class="custom-footer-logo">
+            <img src={twitter} alt="Twitter" />
           </a>
         </li>
         <li>
-          <a href="http://instagram.com/doordash" target="_blank" class="custom-footer-logo">
-            <img src={instagram} />
+          <a href="http://instagram.com/doordash" target="_blank" rel="noreferrer" class="custom-footer-logo">
+            <img src={instagram} alt="Instagram" />
           </a>
         </li>
       </ul>
     </div>
-  </div>
+  </div >
 
 );
